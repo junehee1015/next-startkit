30a3ae24c815b6704acc6e3f6d32039ad470e13e
Juny <junehee1015@iabacus.co.kr>
2026-03-16 16:05:16 +0900

    next-startkit 초기 설정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
 
-export default nextConfig;
+  // 외부 이미지 도메인 설정
+  // images: {
+  //   remotePatterns: [{ protocol: 'https', hostname: '**' }],
+  // },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production' ? { exclude: ['error', 'warn'] } : false,
+  },
+}
+
+export default nextConfig
